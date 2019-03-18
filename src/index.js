fc06ec9338f0674dd2db71fa3562beb0baa43ab9
Volha Zakharava <zakharava.volha@gmail.com>
2019-03-18 13:14:56 +0300

    feat: create logic of function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  //counter for amount of triangles
+  let counter=0;
+  //array to know who have been found in triangle(2 lovers of 3)
+  let trianglePopulation=[];
+  
+    //for-loop to check every member of speachones population
+  for(let index=0; index<preferences.length;index+=1){
+    //the lover of first person -index
+    let firstLover=preferences[index];
+    //the lover of second person
+    let secondLover=preferences[firstLover-1];
+    //the lover of third person
+    let thirdLover=preferences[secondLover-1]
+    //check if third person loves the first one
+    if(((index)==thirdLover-1)){
+      //check if someone from current triangle is registered among existed triangle
+      
+      //index of current triangle is already// not registered in triangle
+      if ((trianglePopulation.indexOf(index)==-1)&&
+          //not loves himself
+        (index!=firstLover-1)){ 
+          //increase counter
+          counter+=1;
+          //register lovers of first and second of the current triangle
+          trianglePopulation.push(firstLover-1);
+          trianglePopulation.push(secondLover-1);
+      }
+    }
+  }
+  return counter;
 };
